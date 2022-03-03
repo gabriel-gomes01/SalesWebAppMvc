6681875df9bc1f1f207d7554b275be47409e2ba7
gabriel-gomes01 <gabrielgomesant@gmail.com>
2022-03-03 18:42:49 -0300

    Outras entidades e segunda migration

--- SalesWebAppMvc/Migrations/20220303214103_OtherEntities.Designer.cs
+++ SalesWebAppMvc/Migrations/20220303214103_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebAppMvc.Data;
 
 namespace SalesWebAppMvc.Migrations
 {
     [DbContext(typeof(SalesWebAppMvcContext))]
-    partial class SalesWebAppMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20220303214103_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SalesWebAppMvc/Models/Enum/SalesStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesWebAppMvc.Models.Enum
+{
+    public enum SalesStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+
+    }
+}
